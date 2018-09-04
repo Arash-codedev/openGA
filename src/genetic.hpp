19be741dd0c48108b01d2d2f64cd0515df388a5b
Arash-codedev <arash.codedev@gmail.com>
2018-09-05 09:43:34 +1000

    2 fixes

--- src/genetic.hpp
+++ src/genetic.hpp
@@ -186,7 +186,7 @@ public:
 	inline double  operator()(uint row,uint col) const {return data[row*n_cols+col];}
 };
 
-double norm2(const std::vector<double> &x_vec)
+inline double norm2(const std::vector<double> &x_vec)
 {
 	double sum=0.0;
 	for(double e:x_vec)
@@ -1420,7 +1420,7 @@ protected:
 		if(is_interactive())
 		{
 			if(N_add+elite_count!=population)
-				throw std::runtime_error("In IGA mode, elite fraction + crossover fraction + mutation fraction must be equal to 1.0 !");
+				throw std::runtime_error("In IGA mode, elite fraction + crossover fraction must be equal to 1.0 !");
 		}
 
 		if(!multi_threading || N_threads==1 || is_interactive())
